70978154bb63d8a398d9ae2e339e85104f2dab98
William Mou <william.mou1024@gmail.com>
2019-12-29 18:45:30 +0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 wget "https://raw.githubusercontent.com/William-Mou/unzi/master/unzi.sh" -O ~/.unzi.sh 
+chmod +x ~/.unzi.sh
 echo "alias unzi="~/.unzi.sh"" >> ~/.bashrc && echo "load bashrc"
 echo "alias unzi="~/.unzi.sh"" >> ~/.zshrc && echo "load zshrc"
 `$SHELL`
